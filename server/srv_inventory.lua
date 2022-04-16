59d0e3571ac5d31c3455ea31f28e70510292968a
Emolitt <emolitt.jolidon@gmail.com>
2022-04-16 16:57:12 +0200

    Addign setup sql scripts

--- server/srv_inventory.lua
+++ server/srv_inventory.lua
@@ -1,9 +1,9 @@
 RegisterCommand("getInv", function(source, args, rawCommand)
     -- If the source is > 0, then that means it must be a player.
     if (source > 0) then
-        local characterId = Core.getUser(source).getUsedCharacter
+        local character = Core.getUser(source).getUsedCharacter
     
-        TriggerClientEvent("vorp:SelectedCharacter", source, characterId)
+        TriggerClientEvent("vorp:SelectedCharacter", source, character.id)
     
     -- If it's not a player, then it must be RCON, a resource, or the server console directly.
     else
